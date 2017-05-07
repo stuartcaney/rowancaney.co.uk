4e4b9aeefe7b6445204b34df6680c3bc702057e8
Stuart Caney <stuartcaney@gmail.com>
2017-05-07 14:34:24 +0100

    fixed server entry point

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var serve = require('koa-static');
 var koa = require('koa');
 var app = new koa();
 
-app.use(serve(__dirname + '/public'));
+app.use(serve(__dirname + '/public/'));
 
 app.listen(3000);
 console.log('listening on port 3000');
